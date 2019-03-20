4f6800c8052a9fce52a43707277c4760426bd8b3
grimgrinn <grimgrinn@gmail.com>
2019-03-20 21:51:43 +0300

    init

---
+++ src/components/layout/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+    <header>
+        <h1>TodoList</h1>
+        <Link to='/'>Home</Link> | &nbsp;
+        <Link to='/about'>About</Link>
+    </header>
+    )
+}
+
+export default Header;
